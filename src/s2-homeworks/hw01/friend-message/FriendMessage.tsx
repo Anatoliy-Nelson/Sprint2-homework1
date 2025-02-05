fd056bf56f7003c29cb88475e2273913142ea5b0
Anatoliy Manzhola <sirius_star@inbox.ru>
2025-02-05 07:53:35 +0300

    first commit

---
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import s from './FriendMessage.module.css'
+
+// создать тип вместо any и отобразить приходящие данные
+export type FriendMessagePropsType = {
+    message: {
+        id: number;
+        user: {
+            avatar: string;
+            name: string;
+        };
+        message: {
+            text: string;
+            time: string;
+        };
+    };
+};
+
+const FriendMessage = (props: FriendMessagePropsType) => {
+    const { message } = props;
+    return (
+        <div
+            id={'hw1-friend-message-' + props.message.id}
+            className={s.friendMessage}
+        >
+            <div className={s.friendImageAndText}>
+                <img
+                    id={'hw1-friend-avatar-' + props.message.id}
+                    // создаёт студент
+                    src={message.user.avatar}
+                    alt="Friend Avatar"
+                    className={s.friendAvatar}
+                    //
+                />
+                <div className={s.friendText}>
+                    <div
+                        id={'hw1-friend-name-' + props.message.id}
+                        className={s.friendName}
+                    >
+                        {/*создаёт студент*/}
+                        {message.user.name}
+                        {/**/}
+                    </div>
+                    <pre
+                        id={'hw1-friend-text-' + props.message.id}
+                        className={s.friendMessageText}
+                    >
+                        {/*создаёт студент*/}
+                        {message.message.text}
+                        {/**/}
+                    </pre>
+                </div>
+            </div>
+            <div
+                id={'hw1-friend-time-' + props.message.id}
+                className={s.friendTime}
+            >
+                {/*создаёт студент*/}
+                {message.message.time}
+                {/**/}
+            </div>
+        </div>
+    )
+}
+
+export default FriendMessage
